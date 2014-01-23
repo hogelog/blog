b0b457aa909747d41f2d7efff2cf31bff34c1753
hogelog <konbu.komuro@gmail.com>
2014-01-23 23:26:32 +0900

    Fix spec

--- spec/config/initializers/secret_token_spec.rb
+++ spec/config/initializers/secret_token_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe Blog::Application do
   describe ".config.secret_key_base" do
     subject { Blog::Application.config.secret_key_base }
-    it { expect(subject).to eq("test_secret_token") }
+    it { expect(subject).to eq("secret_token") }
   end
 end
