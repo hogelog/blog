3742af48f53e0a50399eae47f8dd6dcca6260cd4
hogelog <konbu.komuro@gmail.com>
2014-01-23 23:20:39 +0900

    Fix some admin features

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -39,7 +39,7 @@ class Admin::ArticlesController < AdminController
     @article = Article.find(params[:id])
     @article.destroy
 
-    redirect_to root_path
+    redirect_to admin_path
   end
 
   def preview
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,6 @@
 class Article < ActiveRecord::Base
   belongs_to :user
 
-  has_many :comments, dependent: :destroy
-
   validates :user, presence: true
   validates :title, presence: true
   validates :text, presence: true
